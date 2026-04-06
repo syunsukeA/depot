488a0c16875f2caf2a93071f3c67e0b2b306002f
syunsukeA <shumpei.koshikawa@onecareer.jp>
2026-04-06 21:48:46 +0900

    taskA2 : seed.rbによるデータの自動生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+# . . .
+
+product = Product.create(title: 'Rails Scales!',
+  description:
+    %(<p>
+      <em>Practical Techniques for Performance and Growth</em>
+      Rails doesn't scale. So say the naysayers. They're wrong. Ruby on Rails
+      runs some of the biggest sites in the world, impacting the lives of
+      millions of users while efficiently crunching petabytes of data. This
+      book reveals how they do it, and how you can apply the same techniques
+      to your applications. Optimize everything necessary to make an
+      application function at scale: monitoring, product design, Ruby code,
+      software architecture, database access, caching, and more. Even if your
+      app may never have millions of users, you reduce the costs of hosting
+      and maintaining it.
+      </p>),
+  price: 30.95)
+
+product.image.attach(io: File.open(
+    Rails.root.join('db', 'images', 'cprpo.jpg')),
+    filename: 'cprpo.jpg')
+
+product.save!
+# . . .
