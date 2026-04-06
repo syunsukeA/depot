3ffbb8aef566a9d8ac3daf4fde82d4f5a0252f13
syunsukeA <shumpei.koshikawa@onecareer.jp>
2026-04-06 20:50:11 +0900

    taskA1: 商品テーブルのmigrationファイル修正 & マイグレーション適用

--- db/migrate/20260406105926_create_products.rb
+++ db/migrate/20260406105926_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[8.0]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
