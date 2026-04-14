769e5e918ff27e01d5f8ea4323268eda75df7e9b
syunsukeA <shumpei.koshikawa@onecareer.jp>
2026-04-14 09:38:32 +0900

    商品Modelにバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,19 @@ class Product < ApplicationRecord
 
   # データの更新・削除後に "products" ストリームへリフレッシュ通知を送信する
   after_commit -> { broadcast_refresh_later_to "products" }
+
+  validates :title, :description, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validate :acceptable_image
+
+  def acceptable_image
+  	return unless image.attached?
+
+  	acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
+  	unless acceptable_types.include?(image.content_type)
+    	errors.add(:image, "must be a GIF, JPG or PNG image")
+  	end
+	end
+
 end
